f7ba2c8218e773544f861c5190d8e980fc68068a
Jaivir Sohal <js1424@ic.ac.uk>
2024-12-01 22:13:13 +0000

    Day2 Solutions

---
+++ src/Day2/Main.kt
@@ -0,0 +1,39 @@
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val input = File("src/Day2/input.txt").readLines()
+
+    println("Num of safe reports: ${calcSafe(input)}")
+    println("Num of new safe reports: ${safeDampener(input)}")
+}
+
+fun calcSafe(input: List<String>): Int {
+    val reports = input.map { it.split(" ").map { it.toInt() } }
+    val diffs = reports.map { it.zipWithNext { a, b -> b - a } }
+
+    val numSafe = diffs.filter { isSafe(it) }
+    return numSafe.size
+}
+
+fun safeDampener(input: List<String>): Int {
+    val reports = input.map { it.split(" ").map { it.toInt() } }
+
+    val safeReports = reports.filter { report ->
+        val diffs = report.zipWithNext { a, b -> b - a }
+        if (isSafe(diffs)) {
+            true
+        } else {
+            diffs.indices.any { idx ->
+                val newDiffs = diffs.filterIndexed { index, _ -> index != idx }
+                isSafe(newDiffs)
+            }
+        }
+    }
+
+    return safeReports.size
+}
+
+fun isSafe(diffs: List<Int>): Boolean {
+    return (diffs.all { it > 0 } || diffs.all { it < 0 }) && diffs.all { abs(it) in 1..3 }
+}
